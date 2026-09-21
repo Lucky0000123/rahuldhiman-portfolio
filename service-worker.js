08413e45d53dbe0c04daa788a3ff6321eaa7614b
Rahul Dhiman <lucky@Rahuls-MacBook-Pro-210.local>
2026-09-21 17:19:43 +0700

    Deploy: a11y labels, typo fix, asset cleanup

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rahuldhiman-portfolio/precache-manifest.70b974af38eecac147ac1874fcc15085.js"
+  "/rahuldhiman-portfolio/precache-manifest.b4d854d18a9308b7ba42f969a71b9c0e.js"
 );
 
 self.addEventListener('message', (event) => {
